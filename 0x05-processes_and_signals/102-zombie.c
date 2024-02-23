d764d165d16a4fd229fa280c714a8c16fccd2103
= <mosesodary101@gmail.com>
2024-02-23 22:32:20 +0300

    Final commits

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+/**
+ * main - Make five zombie processes.
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	pid_t pid;
+	char i = 0;
+
+	while (i < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			i++;
+		}
+		else
+			exit(0);
+	}
+
+	while (true)
+	{
+		sleep(1);
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+
