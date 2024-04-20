23b03acc384c92f9039ad0b55d949a543d642077
MosesOdary <mosesodary101@gmail.com>
2024-04-21 00:46:59 +0300

    Slight changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+package { 'Werkzeug':
+  ensure => '2.1.1',  # Specify the version compatible with Flask
+  provider => 'pip3'
+}
